945705cea3c21a3f7e7759540db8e10e591d5e38
Serialbocks <johnj.gw@gmail.com>
2022-03-13 03:44:47 -0500

    Expose act flags to libsm64.h

--- src/libsm64.h
+++ src/libsm64.h
@@ -15,6 +15,29 @@
     #define SM64_LIB_FN
 #endif
 
+#define ACT_FLAG_STATIONARY                  /* 0x00000200 */ (1 <<  9)
+#define ACT_FLAG_MOVING                      /* 0x00000400 */ (1 << 10)
+#define ACT_FLAG_AIR                         /* 0x00000800 */ (1 << 11)
+#define ACT_FLAG_INTANGIBLE                  /* 0x00001000 */ (1 << 12)
+#define ACT_FLAG_SWIMMING                    /* 0x00002000 */ (1 << 13)
+#define ACT_FLAG_METAL_WATER                 /* 0x00004000 */ (1 << 14)
+#define ACT_FLAG_SHORT_HITBOX                /* 0x00008000 */ (1 << 15)
+#define ACT_FLAG_RIDING_SHELL                /* 0x00010000 */ (1 << 16)
+#define ACT_FLAG_INVULNERABLE                /* 0x00020000 */ (1 << 17)
+#define ACT_FLAG_BUTT_OR_STOMACH_SLIDE       /* 0x00040000 */ (1 << 18)
+#define ACT_FLAG_DIVING                      /* 0x00080000 */ (1 << 19)
+#define ACT_FLAG_ON_POLE                     /* 0x00100000 */ (1 << 20)
+#define ACT_FLAG_HANGING                     /* 0x00200000 */ (1 << 21)
+#define ACT_FLAG_IDLE                        /* 0x00400000 */ (1 << 22)
+#define ACT_FLAG_ATTACKING                   /* 0x00800000 */ (1 << 23)
+#define ACT_FLAG_ALLOW_VERTICAL_WIND_ACTION  /* 0x01000000 */ (1 << 24)
+#define ACT_FLAG_CONTROL_JUMP_HEIGHT         /* 0x02000000 */ (1 << 25)
+#define ACT_FLAG_ALLOW_FIRST_PERSON          /* 0x04000000 */ (1 << 26)
+#define ACT_FLAG_PAUSE_EXIT                  /* 0x08000000 */ (1 << 27)
+#define ACT_FLAG_SWIMMING_OR_FLYING          /* 0x10000000 */ (1 << 28)
+#define ACT_FLAG_WATER_OR_TEXT               /* 0x20000000 */ (1 << 29)
+#define ACT_FLAG_THROWING                    /* 0x80000000 */ (1 << 31)
+
 struct SM64Surface
 {
     int16_t type;
