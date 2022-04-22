893803b201c179aa47a568c18392acd44514e833
Serialbocks <johnj.gw@gmail.com>
2022-04-21 22:09:29 -0500

    Fix remote mario wings not fluttering

--- src/decomp/game/mario_misc.c
+++ src/decomp/game/mario_misc.c
@@ -532,9 +532,9 @@ Gfx *geo_mario_rotate_wing_cap_wings(s32 callContext, struct GraphNode *node, UN
         struct GraphNodeRotation *rotNode = (struct GraphNodeRotation *) node->next;
 
         if (!gBodyStates[asGenerated->parameter >> 1].wingFlutter) {
-            rotX = (coss((gAreaUpdateCounter & 0xF) << 12) + 1.0f) * 4096.0f;
+            rotX = (coss((gBlinkUpdateCounter & 0xF) << 12) + 1.0f) * 4096.0f;
         } else {
-            rotX = (coss((gAreaUpdateCounter & 7) << 13) + 1.0f) * 6144.0f;
+            rotX = (coss((gBlinkUpdateCounter & 7) << 13) + 1.0f) * 6144.0f;
         }
         if (!(asGenerated->parameter & 1)) {
             rotNode->rotation[0] = -rotX;
